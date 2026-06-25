b8b53cd376ac7cc606fc352b8a45a9b8c4380b7a
zhangzuoke.fun <zhangzuoke.fun@bytedance.com>
2026-06-25 18:39:57 +0800

    feat: logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths";
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,15 +22,6 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-      traeBadgePlugin({
-        variant: 'dark',
-        position: 'bottom-right',
-        prodOnly: true,
-        clickable: true,
-        clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-        autoTheme: true,
-        autoThemeTarget: '#root'
-      }), 
       tsconfigPaths()
     ],
   };
